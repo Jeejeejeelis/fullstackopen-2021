1c2a5e7e73569cb138c59ec58a59d4000de1177c
Eelis <eelis.pinomaa@gmail.com>
2021-05-20 20:03:21 +0300

    1.6

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.name} </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.text} {props.clicks}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part text = "Good" clicks = {props.good}/>
+      <Part text = "Neutral" clicks = {props.neutral}/>
+      <Part text = "Bad" clicks = {props.bad}/>
+    </div>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const name1 = "give feedback"
+  const name2 = "statistics"
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const setGoodValue = newValue => {
+    console.log(newValue)
+    setGood(newValue)
+  }
+  const setNeutralValue = newValue => {
+    console.log(newValue)
+    setNeutral(newValue)
+  }
+  const setBadValue = newValue => {
+    console.log(newValue)
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <Header name ={name1} />
+      <Button handleClick={() => setGoodValue(good+1)} text="Good" />
+      <Button handleClick={() => setNeutralValue(neutral+1)} text="Neutral" />
+      <Button handleClick={() => setBadValue(bad+1)} text="Bad" />
+      <Header name ={name2} />
+      <Content good = {good} neutral = {neutral} bad = {bad} />
+    </div>
+  )
+}
+
+export default App
