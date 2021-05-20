ad37f971b368d89776f8ce067d6ae21365d08774
Eelis <eelis.pinomaa@gmail.com>
2021-05-20 18:36:00 +0300

    1.5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -34,29 +34,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    }
-  ]
+  const course ={
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      }
+    ]
+  }
   
-  const total = parts[0].exercises+parts[1].exercises+parts[2].exercises
+  const total = course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
 
   return (
     <div>
-      <Header name ={course} />
-      <Content  parts = {parts} />
-      <Total parts ={parts} />
+      <Header name ={course.name} />
+      <Content parts = {course.parts} />
+      <Total parts ={course.parts} />
     </div>
   )
 }
