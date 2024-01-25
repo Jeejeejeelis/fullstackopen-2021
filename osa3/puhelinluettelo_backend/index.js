366c5fc0ce16c8d9ced9315a3b4cc03cf823f289
Jeejeejeelis <eelis.pinoma@gmail.com>
2024-01-25 12:02:46 +0200

    3.15 and 3.16 done

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -5,42 +5,30 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 
-app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
 app.use(cors())
 
 const Person = require('./models/person')
 
-// const mongoose = require('mongoose')
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-// const password = process.argv[2]
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
-// // ÄLÄ KOSKAAN TALLETA SALASANOJA GitHubiin!
-// const url =
-//   `mongodb+srv://eelispinomaa:${password}@cluster0.iklnkwt.mongodb.net/personApp?retryWrites=true&w=majority`
+  next(error)
+}
 
-// mongoose.set('strictQuery',false)
-// mongoose.connect(url)
-
-// const personSchema = new mongoose.Schema({
-//   Name: String,
-//   Number: String,
-// })
-
-// personSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
-// })
-
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 let persons = [
   {
@@ -99,11 +87,17 @@ app.get('/info', (request, response) => {
 //   }
 // })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
   })
-})
 
 // app.delete('/api/persons/:id', (request, response) => {
 //   const id = Number(request.params.id)
@@ -113,7 +107,7 @@ app.get('/api/persons/:id', (request, response) => {
 // })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
+  Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
@@ -145,8 +139,6 @@ const generateId = () => {
 //       error: 'name must be unique'
 //     })
 //   }
-
- 
 
 //   const person = {
 //     name: body.name,
