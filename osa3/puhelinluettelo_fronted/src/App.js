67749234cbace131e8c968b28c327d54379f3e26
Jeejeejeelis <eelis.pinoma@gmail.com>
2024-01-25 05:38:58 +0200

    started working on 3.9 onwards

---
+++ osa3/puhelinluettelo_fronted/src/App.js
@@ -0,0 +1,168 @@
+import React, { useState, useEffect } from 'react'
+//import axios from 'axios'
+import communicationService from './services/Communication'
+
+const Notification = ({ message, type }) => {
+  if (type==='Error'){
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
+  }
+  else if (type==='Success') {
+    return(
+      <div className='success'>
+        {message}
+      </div>
+    )
+  }
+  else{
+    return null
+  }
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      number:'123123'
+    }
+  ])
+
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [errorType, setErrorType] = useState('')
+
+  useEffect(() => {
+    communicationService
+      .getAll()
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const PersonObject = {
+      name: newName,
+      number: newNumber,
+    }
+    if(persons.map(person => person.name.toUpperCase()).indexOf(newName.toUpperCase())!==-1){
+      if(window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)){
+        const person = persons.find(p => p.name === newName)
+        communicationService
+          .update(person.id, PersonObject)
+          .then(response => {
+            setPersons(persons.map(p => p.id !== person.id ? p : response.data))
+            setNewName('')
+            setNewNumber('')
+            setErrorType('Success')
+            setErrorMessage(
+              `${newName}'s number has been changed`
+            )
+            setTimeout(() => {
+              setErrorType(null)
+              setErrorMessage(null)
+            }, 5000)
+          })
+          .catch(error => {
+            setErrorType('Error')
+            setErrorMessage(
+              `Information of ${newName} has already been removed from server`
+            )
+            setTimeout(() => {
+              setErrorType(null)
+              setErrorMessage(null)
+            }, 5000)
+          })
+      }
+    }else{
+      setPersons(persons.concat(PersonObject))
+      setNewName('')
+      setNewNumber('')
+      communicationService
+        .create(PersonObject)
+        .then(response => {
+          console.log(response)
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+          setErrorType('Success')
+          setErrorMessage(
+            `Added ${newName}`
+          )
+          setTimeout(() => {
+            setErrorType(null)
+            setErrorMessage(null)
+          }, 5000)
+        })
+        .catch(error => {
+          setErrorType('Error')
+          setErrorMessage(
+            `Person '${newName}' has not been added`
+          )
+          setTimeout(() => {
+            setErrorType(null)
+            setErrorMessage(null)
+          }, 5000)
+        })
+    }
+  }
+
+  const deletePerson = (id,name) => {
+    if(window.confirm(`delete ${name} ?`)){
+      const person = persons.find(p => p.id === id)
+      communicationService.removePerson(person.id)
+      .then(response => {
+        setPersons(persons.filter(p => p.id !== id))
+      })
+    }
+
+  }
+
+  const filteredPeople = persons.filter(person => person.name.toLowerCase().startsWith(newFilter.toLowerCase()))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={errorMessage} type={errorType} />
+      filter : <input value = {newFilter} onChange={handleFilterChange}>
+      </input>
+      <h2>add a new</h2>
+        <form onSubmit = {addPerson}>
+          name: <input value = {newName} onChange={handleNameChange}
+          />
+          number: <input value = {newNumber} onChange={handleNumberChange}/>
+        <div>
+          <button type="submit">add </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {filteredPeople.map(person =>
+        <p key = {person.id}>{person.name} {person.number} <button onClick={() => deletePerson(person.id,person.name)}>delete</button></p>)}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
---
+++ osa3/puhelinluettelo_fronted/src/services/Communication.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/persons'
+
+const getAll = () => {
+  return axios.get(baseUrl)
+}
+
+const create = newObject => {
+  return axios.post(baseUrl, newObject)
+}
+
+const removePerson = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
+}
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject)
+}
+
+const communicationService = {
+  getAll: getAll,
+  removePerson: removePerson,
+  create: create,
+  update: update
+};
+
+export default communicationService;
